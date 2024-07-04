9717955d54230f717a0ee1138440249528a69dc6
guillermosifu <sifuentes1201@gmail.com>
2024-07-03 22:54:45 -0500

    test

---
+++ semana6/dia1/main.js
@@ -0,0 +1,45 @@
+// iniciacion con js 
+
+// tipos de datos 
+
+
+// number  como se declara nua variable 
+
+// var numero = 15  no se usa por tener un ambito global - conflictos
+
+// number 
+let numero1 = prompt("ingresa valor");
+let numero2=prompt("ingresa el valor 2")
+
+
+
+
+
+const pi = 3.1466685666 
+console.log(numero1)
+// string 
+let alumno = "guillermo";
+
+// booleano 
+let bool= true 
+let bool2 = 0
+
+// null - 
+
+let nada = null
+
+// undefined 
+
+let noDefinido;
+
+// console.log(pi)
+// console.warn("alerta")
+// console.error("error")
+
+//alert ventana emergente  
+
+// alert(132)
+
+//prompt  ventana pero que guarda datos 
+
+//prompt("ingresa tu nombre")
