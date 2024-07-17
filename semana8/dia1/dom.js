0470494cc662811cc51f57c20ea9498b6aeccc8b
guillermosifu <sifuentes1201@gmail.com>
2024-07-16 21:10:21 -0500

    itera

--- semana8/dia1/dom.js
+++ semana8/dia1/dom.js
@@ -28,3 +28,27 @@ console.log(elemento.textContent)
 //getElementsById('nombre')
 //byclassName(nombreClase)
 
+
+const inputName = document.querySelector('.input-email')
+
+console.log(inputName.type)
+
+const password = document.querySelector('#pass')
+console.log(password.value)
+
+const input =document.querySelector('input')
+console.log(input)
+
+
+const inputs = document.querySelectorAll('input')
+console.log(inputs)
+
+
+const convertir = Array.from(inputs)
+console.log(convertir)
+
+const convertirFilter =  convertir.filter(
+    (input) => input.type === 'email'
+);
+
+console.log('iterado',convertirFilter)
