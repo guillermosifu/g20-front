cf6f38acc51e6d7a1ba96c75508ac44185ed3a11
guillermosifu <sifuentes1201@gmail.com>
2024-07-17 20:11:14 -0500

    cambio

--- semana8/dia2/darkmode/darkmode.js
+++ semana8/dia2/darkmode/darkmode.js
@@ -1,5 +1,5 @@
 const Btnprimary = document.querySelector('.btn')
 
 Btnprimary.addEventListener('click',()=>{
-    document.documentElement.classList.toggle('.dark-mode')
+    document.documentElement.classList.toggle('dark-mode')
 });
