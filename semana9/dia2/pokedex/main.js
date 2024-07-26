1048bebd8e56d7275fa0c8808e11f223d53e7ab3
guillermosifu <sifuentes1201@gmail.com>
2024-07-25 21:08:34 -0500

    abilities

--- semana9/dia2/pokedex/main.js
+++ semana9/dia2/pokedex/main.js
@@ -8,7 +8,7 @@
 
 const pokemonsContainer = document.querySelector("#row-pokemons")
 const pokemonName= document.querySelector("#pokemon-name")
-
+const pokemonImg = document.querySelector("#pokemon-img")
 //funcion que consuime los datos del api
 const obtenerPokemones =async()=>{
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
@@ -35,7 +35,7 @@ results.map(async(result,index)=>{
    <h6 class="text-title">N. ${index + 1}</h6>
    <h4 class="text-title">${result.name}</h4>
    </div>
-   <button class="btn btn-danger" onclick="obtenerDetallePokemon("${result.url})" data-bs-toggle="modal" data-bs-target="#modalPokemon">Ver detalle</button>
+   <button class="btn btn-danger" onclick="obtenerDetallePokemon('${result.url}')" data-bs-toggle="modal" data-bs-target="#modalPokemon">Ver detalle</button>
    </div>
    </div>`;
     pokemonsContainer.innerHTML += html;
@@ -48,6 +48,7 @@ const obtenerDetallePokemon=async(url)=>{
   const data= await response.json()
   console.log(data) 
   pokemonName.innerHTML=data.name
+  pokemonImg.src= data.sprites.other.dream_world.front_default
 
 }
 
